4b94a99e32155dbcbf493f38ad3aa3647b9763c9
Firdanbash <firdana.id@gmail.com>
2026-01-01 19:56:12 +0700

    Migrations

---
+++ internal/adapters/postgresql/migrations/00001_create_products.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS products (
+    id BIGSERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    price_in_cent INTEGER NOT NULL CHECK (price_in_cent >= 0),
+    quantity INTEGER NOT NULL DEFAULT 0,
+    created_at TIMESTAMP NOT NULL DEFAULT now()
+);
+-- +goose StatementEnd
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS products;
+-- +goose StatementEnd
