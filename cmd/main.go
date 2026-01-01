940a686e2278d580c014bc619541c38e3f907286
Firdanbash <firdana.id@gmail.com>
2026-01-01 14:37:07 +0700

    Structured logging

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"log"
+	"log/slog"
 	"os"
 )
 
@@ -14,8 +14,12 @@ func main() {
 		config: cfg,
 	}
 
+	// Structured logging
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
 	if err := api.run(api.mount()); err != nil {
-		log.Printf("server failed to start, err: %s", err)
+		slog.Error("Server failed to start", "error", err)
 		os.Exit(1)
 	}
 }
